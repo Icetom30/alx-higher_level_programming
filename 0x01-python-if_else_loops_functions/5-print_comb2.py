3578ff3468d2005c4ca116f2233218dc5203ff8d
Tswain <isaactom55@gmail.com>
2022-06-28 11:48:25 -0700

    00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,7 @@
-
-
 #!/usr/bin/python3
-for i in range(0, 100):
-    print("{:02d}".format(i), end="\n" if i == 99 else ", ")
+
+for number in range(0, 100):
+    if number == 99:
+        print("{}".format(number))
+    else:
+        print("{:02}".format(number), end=", ")
